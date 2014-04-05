fdbca79d8bcb0ff0afd9dec433211bcd88e17008
James <hailfire91@gmail.com>
2014-04-05 04:14:12 -0500

    added a List of stations to the battle ship. needs to be tested with station instantated to insure functionality

--- Assets/Scripts/Battleship.cs
+++ Assets/Scripts/Battleship.cs
@@ -1,16 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 public class Battleship : MonoBehaviour {
 
 	private List<CrewMember> crewMembers;
+	private List<Station> stations;
 
 	// Use this for initialization
 	void Start () {
 		crewMembers = new List<CrewMember> ();
+		stations = new List<Station> ();
 		//For debugging, create a crew member
 		crewMembers.Add (Instantiate (Resources.Load ("Prefabs/CrewMember")) as CrewMember);
+
+		//find all game objects htat have stations and add them to a list.
+		GameObject[] objectArr = GameObject.FindObjectsOfType(typeof(MonoBehaviour)) as GameObject[];
+		if (objectArr != null) 
+		{
+			foreach (GameObject obj in objectArr) 
+			{
+				Station script = obj.GetComponent (typeof(Station)) as Station;
+				if (script != null) 
+				{
+					stations.Add (script);
+				}
+			}
+		}
+		//stations = GameObject.FindObjectsOfType (Station) as Station[];
+		Debug.Log (stations.ToString());
 	}
 	
 	// Update is called once per frame
