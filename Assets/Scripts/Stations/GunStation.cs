6cf23d400cd939c3d8102204b4dcfffca520a265
James <hailfire91@gmail.com>
2014-04-04 23:02:15 -0500

    added the ability to add and remove crew to stations

--- Assets/Scripts/Stations/GunStation.cs
+++ Assets/Scripts/Stations/GunStation.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+//using System.Collections.ArrayList;
 
 public class GunStation : MonoBehaviour, Station {
 	int health;
-
+	List<CrewMember> crewList;
 
 	// Use this for initialization
 	void Start () {
 		health = 0;
+		crewList = new List<CrewMember> ();
 	}
 	
 	// Update is called once per frame
@@ -29,5 +32,20 @@ public class GunStation : MonoBehaviour, Station {
 	public Vector2 getTarget(CrewMember crewIn)
 	{
 		return this.transform.position;
+	}
+
+	public void addCrew(CrewMember crewIn)
+	{
+		if (crewIn != null)
+		{
+			crewList.Add(crewIn);
+		}
+	}
+	public void removeCrew(CrewMember crewIn)
+	{
+		if(crewList.Contains(crewIn));
+		{
+			crewList.Remove(crewIn);
+		}
 	}
 }
