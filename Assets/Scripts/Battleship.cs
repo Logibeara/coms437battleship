8ff2a7c92d0d892fda10a4b6de2b0215096d6e7e
Logan Scott <logscott@iastate.edu>
2014-04-05 05:12:57 -0500

    repeated merging

--- Assets/Scripts/Battleship.cs
+++ Assets/Scripts/Battleship.cs
@@ -6,12 +6,8 @@ using System.Linq;
 public class Battleship : MonoBehaviour {
 
 	private List<CrewMember> crewMembers;
-<<<<<<< HEAD
 	private int numCrewMembers = 10;
-=======
 	private List<Station> stations;
->>>>>>> fdbca79d8bcb0ff0afd9dec433211bcd88e17008
-
 	// Use this for initialization
 	void Start () {
 
@@ -19,20 +15,16 @@ public class Battleship : MonoBehaviour {
 			transform.FindChild("CrewSpawner1").position,
 			transform.FindChild("CrewSpawner2").position};
 		crewMembers = new List<CrewMember> ();
-<<<<<<< HEAD
 
 		for(int i = 0; i < numCrewMembers; i ++)
 		{
 			CrewMember crewMember = (
 				Instantiate (Resources.Load ("Prefabs/CrewMember")) as GameObject).GetComponent(
 				typeof(CrewMember)) as CrewMember;
-			Debug.Log("before : " + crewMember.Position );
 			crewMember.Position = crewPos[i%crewPos.Length];
-			Debug.Log("after : " + crewMember.Position );
 			crewMembers.Add(crewMember);
 		}
 
-=======
 		stations = new List<Station> ();
 		//For debugging, create a crew member
 		crewMembers.Add (Instantiate (Resources.Load ("Prefabs/CrewMember")) as CrewMember);
@@ -52,7 +44,6 @@ public class Battleship : MonoBehaviour {
 		}
 		//stations = GameObject.FindObjectsOfType (Station) as Station[];
 		Debug.Log (stations.ToString());
->>>>>>> fdbca79d8bcb0ff0afd9dec433211bcd88e17008
 	}
 	
 	// Update is called once per frame
