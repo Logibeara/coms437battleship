5f07f61300f80fff09ebcb9b3aa73e18637c790b
James <hailfire91@gmail.com>
2014-04-04 21:51:13 -0500

    added an aspect ratio for vertical phones. also added more things to the Station interface and gunstation class. specifically health.

---
+++ Assets/Scripts/Stations/GunStation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunStation : MonoBehaviour, Station {
+	int health;
+
+
+	// Use this for initialization
+	void Start () {
+		health = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public int Health{ 
+		get
+		{
+			return health;
+		}
+		set
+		{
+			health = value;
+		}
+	}
+
+	public Vector2 getTarget(CrewMember crewIn)
+	{
+		return this.transform.position;
+	}
+}
