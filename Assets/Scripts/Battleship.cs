6816860b79538d61d869ef715f045cc27dee1756
Logan Scott <logscott@iastate.edu>
2014-04-05 05:01:57 -0500

    added multiple spawn points for crew member spawning

--- Assets/Scripts/Battleship.cs
+++ Assets/Scripts/Battleship.cs
@@ -5,12 +5,27 @@ using System.Collections.Generic;
 public class Battleship : MonoBehaviour {
 
 	private List<CrewMember> crewMembers;
+	private int numCrewMembers = 10;
 
 	// Use this for initialization
 	void Start () {
+
+		Vector3[] crewPos = {
+			transform.FindChild("CrewSpawner1").position,
+			transform.FindChild("CrewSpawner2").position};
 		crewMembers = new List<CrewMember> ();
-		//For debugging, create a crew member
-		crewMembers.Add (Instantiate (Resources.Load ("Prefabs/CrewMember")) as CrewMember);
+
+		for(int i = 0; i < numCrewMembers; i ++)
+		{
+			CrewMember crewMember = (
+				Instantiate (Resources.Load ("Prefabs/CrewMember")) as GameObject).GetComponent(
+				typeof(CrewMember)) as CrewMember;
+			Debug.Log("before : " + crewMember.Position );
+			crewMember.Position = crewPos[i%crewPos.Length];
+			Debug.Log("after : " + crewMember.Position );
+			crewMembers.Add(crewMember);
+		}
+
 	}
 	
 	// Update is called once per frame
