91837e1c9d3895e79efe5de27b7e4d86587cf169
Logan Scott <logscott@iastate.edu>
2014-04-23 13:44:23 -0500

    adding MainGun tag to scene1 guns, and started 3d battleship behavior script

---
+++ Assets/ExBattleship.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExBattleship : MonoBehaviour {
+	private GameObject exMainGun1;
+	private GameObject exMainGun2;
+	private GameObject exMainGun3;
+	
+	
+	private GameObject mainGun1;
+	private GameObject mainGun2;
+	private GameObject mainGun3;
+	// Use this for initialization
+	void Start () {
+		//find all game objects htat have stations and add them to a list.
+		exMainGun1 = GameObject.Find("ExBattleshipExMainGun1") as GameObject;
+		exMainGun2 = GameObject.Find("ExBattleshipExMainGun2") as GameObject;
+		exMainGun3 = GameObject.Find("ExBattleshipExMainGun3") as GameObject;
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mainGun1 == null || mainGun2 == null || mainGun3 == null) {
+			GameObject[] objarr = GameObject.FindGameObjectsWithTag ("MainGun") as GameObject[];
+			
+			if (objarr != null) {
+				mainGun1 = objarr [0];
+				mainGun2 = objarr [1];
+				mainGun3 = objarr [2];
+			}
+		}
+	}
+}
