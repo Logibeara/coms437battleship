10742ac6c7c7f08c04a8ba34109d1bebe9717cc3
Logan Scott <logscott@iastate.edu>
2014-04-23 16:46:30 -0500

    added beginning enemy battleship with animation

---
+++ Assets/EnemyBattleship.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBattleship : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
