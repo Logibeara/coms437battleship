aee524faac7423b162970281c41f76a28a881675
James <hailfire91@gmail.com>
2014-04-22 23:24:03 -0500

    added healthe and the ability to kill a fire. still need to impliment destroy...

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class Fire : MonoBehaviour {
 
+	float health;
+	int workTicker;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		health = 1000;
+		workTicker = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		health -= workTicker * Time.deltaTime;
+		if(health <= 0)
+		{
+			destroy();
+		}
+	}
+
+	public int Health
+	{
+		get
+		{
+			return health;
+		}
+		set
+		{
+			health = value;
+		}
+	}
+
+	private void destroy()
+	{
+		//remove from the fire list on the battle ship
+
 	}
 }
