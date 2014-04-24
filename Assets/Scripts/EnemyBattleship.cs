d4d8d092cbd2774471b62937bad5c3ef77626256
Logan Scott <logscott@iastate.edu>
2014-04-24 12:11:56 -0500

    Added ship hit/death explosion particle system

--- Assets/Scripts/EnemyBattleship.cs
+++ Assets/Scripts/EnemyBattleship.cs
@@ -5,13 +5,15 @@ public class EnemyBattleship : MonoBehaviour {
 	private double startingHealth = 500;
 	public double currentHealth;
 	// Use this for initialization
-
+	ShipExplosionEffect effect;
 
 	void Start () {
 		//mainGun1 = GameObject.Find("EnemyBattleshipExMainGun1") as GameObject;
 		//mainGun2 = GameObject.Find("EnemyBattleshipExMainGun2") as GameObject;
 		//mainGun3 = GameObject.Find("EnemyBattleshipExMainGun3") as GameObject;
 		currentHealth = startingHealth;
+		
+		effect = this.GetComponent<ShipExplosionEffect> ();
 	}
 	
 	// Update is called once per frame
@@ -21,9 +23,14 @@ public class EnemyBattleship : MonoBehaviour {
 
 	public void DoDamage(double amount)
 	{
-		currentHealth -= amount;
-	}
-
+				currentHealth -= amount;
+				effect.SmallHit ();
+				if (currentHealth <= 0) {
+		
+						currentHealth = 0;
+						effect.KillExplosion ();
+				}
+		}
 	public double GetCurrentHealth()
 	{
 		return currentHealth;
---
+++ Assets/Scripts/ShipExplosionEffect.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipExplosionEffect : MonoBehaviour {
+		
+		private Transform hitPos1;
+		private Transform hitPos2;
+		private Transform hitPos3;
+		
+		public Object partSys;
+		
+		private GameObject explosion1;
+		private GameObject explosion2;
+		private GameObject explosion3;
+		
+		public AudioSource gunExplosionSound;
+		
+		
+		//sound
+		
+		// Use this for initialization
+		void Start () {
+			string pname =  this.transform.parent.name;
+			hitPos1 = this.transform.FindChild("HitPos1");
+			hitPos2 = this.transform.FindChild("HitPos2");
+			hitPos3 = this.transform.FindChild("HitPos3");
+			
+			//partSys = Resources.Load ("particle/Explosion06");
+			gunExplosionSound = this.GetComponent<AudioSource> ();
+			
+		}
+		
+		
+	int hitCounter = 0;
+		public void SmallHit()
+		{
+			
+		if (hitCounter == 0) {
+			explosion1 = Instantiate (partSys, hitPos1.position, hitPos1.rotation) as GameObject;
+			Destroy (explosion1, 10);
+		}else if(hitCounter == 1)
+		{
+			explosion2 = Instantiate (partSys, hitPos2.position, hitPos2.rotation) as GameObject;
+			Destroy (explosion2, 10);
+		}else if(hitCounter == 2)
+		{
+			explosion3 = Instantiate (partSys, hitPos3.position, hitPos3.rotation) as GameObject;
+			Destroy (explosion3, 10);
+
+		}
+		
+			hitCounter += Random.Range(0,3);
+			hitCounter = hitCounter % 3;
+		}
+	public void KillExplosion()
+	{
+			
+
+
+			explosion1 = Instantiate (partSys, hitPos1.position, hitPos1.rotation) as GameObject;
+		//	explosion1.GetComponent<ParticleSystem> ().startSpeed = 1;
+			Destroy (explosion1, 10);
+			explosion2 = Instantiate (partSys, hitPos2.position, hitPos2.rotation) as GameObject;
+		//	explosion2.GetComponent<ParticleSystem> ().startSpeed = 1;
+			Destroy (explosion2, 10);
+			explosion3 = Instantiate (partSys, hitPos3.position, hitPos3.rotation) as GameObject;explosion1.GetComponent<ParticleSystem> ().startSpeed = 1;
+		//	explosion3.GetComponent<ParticleSystem> ().startSpeed = 1;
+			Destroy (explosion3, 10);
+			
+	}
+
+}
